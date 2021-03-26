a10b688943c2049fa5b592d63da1d8d2af5b0f1a
ctalk <rk3314042@gmail.com>
2021-03-25 20:14:22 -0700

    Added DBUtil class to check for existing schema and create user table if needed.

---
+++ projectOne/projectOne/target/generated-sources/annotations/com/revature/rkiesling/project1/Login.java
@@ -0,0 +1,57 @@
+package com.revature.rkiesling.project1;
+
+import com.revature.rkiesling.project1.util.JDBCConnection;
+import com.revature.rkiesling.project1.util.DBUtil;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+       
+        public void init() throws ServletException {
+                try {
+                        JDBCConnection.registerPostGresqlDriver ();
+                } catch (SQLException e) {
+                        System.out.println (e.getMessage ());
+                        throw new ServletException (e.getMessage());
+                }
+        }
+        
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                
+                Connection con = JDBCConnection.getJDBCConnection ();
+                response.setContentType ("text/html");
+                PrintWriter out = response.getWriter ();
+                // Check whether we have a database
+                if (!DBUtil.haveSchema ()) {
+                   out.append(DBUtil.makeSchema ());
+		   out.append(DBUtil.makeTables ());
+                }
+                JDBCConnection.closeAll(con);
+                
+                
+        }
+
+}
---
+++ projectOne/projectOne/target/generated-sources/annotations/com/revature/rkiesling/project1/Role.java
@@ -0,0 +1,7 @@
+package com.revature.rkiesling.project1;
+
+public interface Role {
+	public static final int ROLE_ADMIN = 0;
+	public static final int ROLE_EMPLOYEE = 1;
+	public static final int ROLE_FINANCE = 2;
+}
